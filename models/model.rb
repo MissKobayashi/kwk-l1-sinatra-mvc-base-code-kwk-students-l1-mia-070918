6d68b708340eda2ef314d5f167dab0d2a4521e39
MissKobayashi <priscin2002@gmail.com>
2018-07-17 15:52:08 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -1,4 +1,4 @@
-def get_fotrune
+def get_fortune
   fortune_array = ["you will proster", "you will the the vegetable off today",
   "you will get to meet Karlie Kloss", "you will meet your lover", "you will loss what you hold dear"]
 end
