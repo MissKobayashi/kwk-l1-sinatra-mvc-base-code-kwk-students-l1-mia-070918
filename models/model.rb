f9106cab9b3565f3e96779410bf2851d48074930
MissKobayashi <priscin2002@gmail.com>
2018-07-17 15:53:12 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -1,4 +1,5 @@
 def get_fortune
   fortune_array = ["you will proster", "you will the the vegetable off today",
   "you will get to meet Karlie Kloss", "you will meet your lover", "you will loss what you hold dear"]
+  return fortune_array.sample
 end
